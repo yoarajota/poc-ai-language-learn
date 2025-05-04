3e9df0d7566a619b85a025ecb5426d449848b436
yoarajota <joaovbscontato@gmail.com>
2025-05-03 22:54:47 -0300

    fix: remove useless endpoints (no prod app)

--- service/app/main.py
+++ service/app/main.py
@@ -60,15 +60,3 @@ async def websocket_endpoint(websocket: WebSocket):
     except Exception as e:
         print(f"Error: {e}")
         await websocket.close()
-
-def test_read_main():
-    client = TestClient(app)
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
-
-def test_websocket():
-    client = TestClient(app)
-    with client.websocket_connect("/ws") as websocket:
-        data = websocket.receive_json()
-        assert data == {"msg": "Hello WebSocket"}
